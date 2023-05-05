3e9262937187508e902d9710b3f5aa8a3086dd7e
“[Nic <[nicjcoxen@gmail.com]>
2023-05-06 10:40:36 +1200

    initial

---
+++ Boston weather.R
@@ -0,0 +1,16 @@
+library(knitr)
+knitr::opts_chunk$set(echo = FALSE)
+knitr::opts_chunk$set(message = FALSE)
+opts_chunk$set(comment="", fig.align="center", tidy=TRUE , cache=TRUE)
+library(tidyverse)
+library(lattice)
+library(data.table)
+library(GGally)
+
+
+Boston <- read_csv("C:/Data/Boston weather.csv")
+
+
+
+write.csv(Boston,"Boston_weather.csv",row.names=FALSE)
+
