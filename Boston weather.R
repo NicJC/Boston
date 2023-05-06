a3dec6e8693d3d9d8067635922da6f60f3e357cc
“[Nic <[nicjcoxen@gmail.com]>
2023-05-06 12:04:02 +1200

    update

--- Boston weather.R
+++ Boston weather.R
@@ -14,3 +14,12 @@ Boston <- read_csv("C:/Data/Boston weather.csv")
 
 write.csv(Boston,"Boston_weather.csv",row.names=FALSE)
 
+ggpairs(Boston[,c(1,4,5,6)],
+        mapping = ggplot2::aes(color = Year),
+        upper = list(continuous = wrap("density", alpha = 0.5), combo = "box_no_facet"))+ggplot2::labs(title = "Boston Weather")  + 
+  theme(axis.text.x = element_text(color="steelblue", 
+                                   size=12, angle=90),
+        axis.text.y = element_text( color="steelblue", 
+                                    size=12))
+
+ggsave("matrixPlot.png", width = 6, height = 6)
